74ab3d24d015b0e661480c969da0c486a692f521
ZeekoZhu <vaezt@outlook.com>
2019-03-24 15:40:20 +0800

    update backup script

--- shell/full_system_backup.fish
+++ shell/full_system_backup.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/fish
-rsync -aAXH --inplace --info=progress2 --delete --exclude={"/dev/*","/proc/*", "/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.nuget/*", "/home/*/.cache/*"} / $argv
+rsync -aAXH --inplace --info=progress2 --delete --exclude={"/swapfile","/dev/*","/proc/*", "/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.nuget/*", "/home/*/.cache/*"} / $argv
 
