e7e1873bd01e8c2d299552c1c4b194c234e0b8cf
ZeekoZhu <vaezt@outlook.com>
2019-03-28 22:49:44 +0800

    add more configs

--- dotFiles/.config/fish/conf.d/zeeko.fish
+++ dotFiles/.config/fish/conf.d/zeeko.fish
@@ -4,9 +4,12 @@ alias stlR="sudo systemctl daemon-reload"
 alias stlp="sudo systemctl stop"
 alias stl="sudo systemctl status"
 alias pc="proxychains4"
+alias fsi="dotnet fsi --readline"
 fish_vi_key_bindings
 
 set -gx DOTNET_ROOT /opt/dotnet
 set -gx MSBuildSDKsPath /opt/dotnet/sdk/(dotnet --version)/Sdks
 set -gx PATH $HOME/.yarn/bin $HOME/.cargo/bin $HOME/.dotnet/tools $PATH
+set -gx HTTP_PROXY http://localhost:8001
+set -gx HTTPS_PROXY http://localhost:8001
 
